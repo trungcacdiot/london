e1a123571fbce514de9b0cb088c8fbf83fbc9dce
trungbui <buibatrung01@gmail.com>
2020-09-12 09:02:32 +0700

    Updates

---
+++ precache-manifest.daa4550383e11b2cba5c90ecca0585b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8543619dba912cc6e98198aad875ab",
+    "url": "/london/index.html"
+  },
+  {
+    "revision": "da2fda025923f52399a6",
+    "url": "/london/static/css/main.da87f4dc.chunk.css"
+  },
+  {
+    "revision": "f4759dfaf791c4de808a",
+    "url": "/london/static/js/2.8c6d7dcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/london/static/js/2.8c6d7dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2fda025923f52399a6",
+    "url": "/london/static/js/main.c6890f09.chunk.js"
+  },
+  {
+    "revision": "c770394d6a4dc61f65f5",
+    "url": "/london/static/js/runtime-main.5d110b8c.js"
+  },
+  {
+    "revision": "a3a103e6a9f6d977c50e9b72ca005d6a",
+    "url": "/london/static/media/london.a3a103e6.jpg"
+  }
+]);
