84ce4bb661141c5bb83d3c59523ecae81ec42a78
trungbui <buibatrung01@gmail.com>
2020-09-14 23:02:48 +0700

    'fine'

--- src/components/secondPage/Content.js
+++ src/components/secondPage/Content.js
@@ -6,13 +6,13 @@ export default class Content extends Component {
       <div className="content__container">
         <div>
           {" "}
-          <span className="experience">Experience</span>{" "}
-          <span className="lage_font">a trip like no other</span> Lorem ipsum
+          <span className="experience">Experience<br/></span>{" "}
+          <span className="lage_font">a trip like no other<br/><br/></span> Lorem ipsum
           dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
           tempor invidunt ut labore et labore et dolore magna aliquyam erat, sed
-          diam voluptua. At vero eos et accusam. stet clita kasd gubergren, no
+          diam voluptua. At vero eos et accusam. <br/><br/>stet clita kasd gubergren, no
           sea takimata sanctus est Lorem ipsum dolor sit met. Lorem ipsum dolor
-          sit met, consetetur sadipscing elitr.
+          sit met, consetetur sadipscing elitr.<br/><br/>
         </div>
       </div>
     );
