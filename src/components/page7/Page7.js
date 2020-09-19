481240a2b79591affea7ecec8f19235c80bbc110
trungcacdiot <68458523+trungcacdiot@users.noreply.github.com>
2020-09-19 16:41:14 +0700

    page7

--- src/components/page7/Page7.js
+++ src/components/page7/Page7.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 import "./Page7.css";
 class Page7 extends Component {
   render() {
-    return <div className="page7"></div>;
+    return (
+      <div className="page7">
+        <div className="page7__gradient">
+          <div className="Page7__left">
+            <div className="p1"> Partner with</div>
+            <div className="p2">Tailored Experiences</div>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua:
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
